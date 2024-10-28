47bcc84918f5e3a4c4f03bea55f4516301edbdf6
GH23891 <GH23891@Surf.com>
2024-10-28 16:08:41 +0200

    change import

--- libs/shared/ui/src/lib/reusable-table/reusable-table.component.ts
+++ libs/shared/ui/src/lib/reusable-table/reusable-table.component.ts
@@ -1,10 +1,10 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
-import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {AfterViewInit, Component, Input, ViewChild, inject} from '@angular/core';
-import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {BaseTable} from '../../../../../types/src/lib/types/types'
+import { AfterViewInit, Component, Input, ViewChild, inject } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { BaseTable } from '@cms-monoripo/types';
 
 @Component({
   selector: 'lib-reusable-table',
@@ -33,7 +33,6 @@ export class ReusableTableComponent implements AfterViewInit {
       this.dataSource.paginator = this.paginator;
     }
   }
-
 
   announceSortChange(sortState: Sort) {
     if (sortState.direction) {
